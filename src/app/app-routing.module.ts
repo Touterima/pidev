dbfef95f756b8cd35362ad8476d31ec34a25916f
rima <rtoute311@gmail.com>
2025-02-12 21:04:21 +0100

    templates integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AcceuilLayoutComponent } from './layouts/acceuil-layout/acceuil-layout.component';
+import { AcceuilComponent } from './pages/acceuil/acceuil.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardLayoutComponent, // Page Home avec Starter ButterCMS
+    component: DashboardLayoutComponent, // Page Home avec la template adminlte
     children: [
       { path: '', component: DashboardComponent }
+    ]
+  },
+  {
+    path: '',
+    component: AcceuilLayoutComponent, // Page Home avec la template 2 Starter ButterCMS
+    children: [
+      { path: '', component: AcceuilComponent }
     ]
   }
 ];
